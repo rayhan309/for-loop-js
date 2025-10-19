a72c62a033916e6040a2af174c4b8b829c38246b
Abu Rayhan <ihaveawonderfull@gmail.com>
2025-10-19 16:09:16 -0700

    first commit

--- p-1.js
+++
@@ -1,6 +0,0 @@
-let sms = 1;
-
-while (sms <= 60) {
-    console.log('sms count:', sms);
-    sms++;
-}
--- while-loop.js
+++
@@ -1,9 +0,0 @@
-let num = 1;
-let sum = 0;
-
-while (num <= 5) {
-    console.log('hello word:', num);
-    sum += num;
-    console.log('sum:', sum);
-    num++;
-}
